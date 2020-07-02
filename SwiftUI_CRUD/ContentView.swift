ecf36f9b42c575d9491554e1967e31e673a8cbd6
Henry Havens <hhavens@hotmail.com>
2020-07-02 09:20:57 -0500

    Initial Commit

---
+++ SwiftUI_CRUD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_CRUD
+//
+//  Created by Henry Havens on 7/2/20.
+//  Copyright © 2020 Henry Havens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
